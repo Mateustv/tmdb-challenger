fa81c9982ab58001a77c1caacd81c8e48cda5ac5
Mateus T <mateustavarestv@hotmail.com>
2022-01-25 14:53:45 -0300

    requisição a api feita

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BannerPrincipal } from '../Components/BannerPrincipal';
 import { Header } from '../Components/Header'
+import { api } from '../utils/api';
 
 import styles from './homePage.module.scss'
 
 export default function HomePage() {
   const [theme, setTheme] = useState('')
+  const [movies, setMovies] = useState([])
+
+  useEffect(async () => {
+    const { data } = await api.get('/movie/popular?api_key=1b62339a09333a86b64565261d79c698')
+    const results = data.results.map(result => {
+      return {
+        id: result.id,
+        genre_ids: result.genre_ids,
+        original_title: result.original_title,
+        poster_path: result.poster_path,
+        release_date: result.release_date,
+        title: result.title,
+      }
+    })
+
+    setMovies(results)
+    console.log(results, data)
+  }, [])
+
   return (
     <>
       <Header />
@@ -13,41 +33,13 @@ export default function HomePage() {
       <section className={styles.containerImages}>
         <div className={styles.listImages}>
           <ul>
-            <li>
-              <img src="/images/image1.png" alt="imagem" />
-              <p>Oi, Alberto</p>
-              <span>12 NOV 2021</span>
-            </li>
-            <li>
-              <img src="/images/image1.png" alt="imagem" />
-              <p>Oi, Alberto</p>
-              <span>12 NOV 2021</span>
-            </li>
-            <li>
-              <img src="/images/image1.png" alt="imagem" />
-              <p>Oi, Alberto</p>
-              <span>12 NOV 2021</span>
-            </li>
-            <li>
-              <img src="/images/image1.png" alt="imagem" />
-              <p>Oi, Alberto</p>
-              <span>12 NOV 2021</span>
-            </li>
-            <li>
-              <img src="/images/image1.png" alt="imagem" />
-              <p>Oi, Alberto</p>
-              <span>12 NOV 2021</span>
-            </li>
-            <li>
-              <img src="/images/image1.png" alt="imagem" />
-              <p>Oi, Alberto</p>
-              <span>12 NOV 2021</span>
-            </li>
-            <li>
-              <img src="/images/image1.png" alt="imagem" />
-              <p>Oi, Alberto</p>
-              <span>12 NOV 2021</span>
-            </li>
+            {movies.map(data => (
+              <li key={data.id}>
+                <img src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`} alt={data.title} />
+                <p>{data.title}</p>
+                <span>{data.release_date}</span>
+              </li>
+            ))}
           </ul>
         </div>
         <div className={styles.paginationNumbers}>
